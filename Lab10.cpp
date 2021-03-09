4c26467dc109a05c9bdb3d71570173c000581c13
guerrk1n <aleksej684549@inbox.ru>
2021-03-09 17:57:46 +0300

    search and sort alg done

--- Lab10.cpp
+++ Lab10.cpp
@@ -5,15 +5,6 @@
 #include <string>
 
 using namespace std;
-void createFile();
-void printData();
-void writeFile();
-void readFile();
-void info();
-void openFile(string);
-void sort();
-void quicksort();
-void insertionsort();
 string location();
 FILE* file;
 string locationName;
@@ -25,6 +16,17 @@ struct railway {
 	int time;
 };
 railway railwayArray[5];
+void createFile();
+void printData();
+void writeFile();
+void readFile();
+void info();
+void openFile(string);
+void insertionsort();
+void quickSort(railway*, int, int);
+void bruteForceSearch();
+void binarySearch();
+void print();
 
 int main()
 {
@@ -34,8 +36,11 @@ int main()
 			<< "\t2 - Print data\n"
 			<< "\t3 - Write data\n"
 			<< "\t4 - Info about free seat to destination\n"
-			<< "\t5 - Sort\n"
-			<< "\t6 - Exit\n" << endl;
+			<< "\t5 - Insertion Sort\n"
+			<< "\t6 - Quick Sort\n"
+			<< "\t7 - Brute - force search\n"
+			<< "\t8 - Binary search\n"
+			<< "\t10 - Exit\n" << endl;
 		switch (_getch()) {
 		case '1':
 			createFile();
@@ -50,12 +55,29 @@ int main()
 			info();
 			break;
 		case '5':
-			sort();
+			insertionsort();
 			break;
 		case '6':
+			openFile("r");
+			readFile();
+			quickSort(railwayArray, 0, fieldsAmount-1);
+			print();
+			fclose(file);
+			break;
+		case '7':
+			bruteForceSearch();
+			break;
+		case '8':
+			openFile("r");
+			readFile();
+			quickSort(railwayArray, 0, fieldsAmount - 1);
+			binarySearch();
+			fclose(file);
+			break;
+		case '10':
 			exit(0);
 			break;
-		default: 
+		default:
 			printf("Check your input \n");
 			break;
 		}
@@ -67,14 +89,19 @@ void createFile() {
 	printf("File created \n");
 }
 
+void print() {
+	for (int i = 0; i < fieldsAmount; i++) {
+	printf("%s %d %d %d\n", railwayArray[i].finalDestination, railwayArray[i].numberTrain,
+		railwayArray[i].freeSeats, railwayArray[i].time);
+}
+}
+
 void printData() {
 
-		openFile("r");
-		readFile();
-		for(int i=0; i < fieldsAmount; i++)
-		printf("%s %d %d %d\n", railwayArray[i].finalDestination, railwayArray[i].numberTrain,
-			railwayArray[i].freeSeats, railwayArray[i].time);
-		fclose(file);
+	openFile("r");
+	readFile();
+	print();
+	fclose(file);
 }
 
 void openFile(string param) {
@@ -83,11 +110,12 @@ void openFile(string param) {
 			printf("Error while opening \n");
 			exit(1);
 		}
-	}else if ((file = fopen(location().c_str(), param.c_str())) == NULL) {
+	}
+	else if ((file = fopen(location().c_str(), param.c_str())) == NULL) {
 		printf("Error while opening \n");
 		exit(1);
 	}
-	
+
 }
 
 string location() {
@@ -97,11 +125,11 @@ string location() {
 }
 
 void readFile() {
-	
+
 	fieldsAmount = 0;
 	rewind(file);
 	while (!feof(file)) {
-		fscanf(file," %s %d%d%d\n", &railwayArray[fieldsAmount].finalDestination,&railwayArray[fieldsAmount].numberTrain,
+		fscanf(file, " %s %d%d%d\n", &railwayArray[fieldsAmount].finalDestination, &railwayArray[fieldsAmount].numberTrain,
 			&railwayArray[fieldsAmount].freeSeats, &railwayArray[fieldsAmount].time);
 		fieldsAmount++;
 	}
@@ -111,13 +139,13 @@ void writeFile() {
 	openFile("w");
 	string input;
 	printf("Enter CTRL + Z to exit \n");
-		while (getline(cin, input)) {
-			printf("Destination,Train number, Free Seats, Time: ");
-			fputs(input.c_str(), file);
-		}
-			fclose(file);
-		}
-	
+	while (getline(cin, input)) {
+		printf("Destination,Train number, Free Seats, Time: ");
+		fputs(input.c_str(), file);
+	}
+	fclose(file);
+}
+
 
 void info() {
 	string destination;
@@ -126,7 +154,7 @@ void info() {
 	printf("Enter destination: ");
 	cin >> destination;
 	bool freeSeats = false;
-	printf("Avaible trains to: %s \n",destination);
+	printf("Avaible trains to: %s \n", destination);
 	for (int i = 0; i < fieldsAmount; i++) {
 		if (destination == railwayArray[i].finalDestination && railwayArray[i].freeSeats > 0) {
 			freeSeats = true;
@@ -139,27 +167,22 @@ void info() {
 	}
 }
 
-void sort() {
-	/*cout << "Make a choise.\n"
-		<< "\t1 - QuickSort\n"
-		<< "\t2 - InsertionSort\n" << endl;
-	switch (_getch())
-	{
-	case '1':
-		quicksort();
-		break;
-	case '2':
-		insertionsort();
-		break;
-	default:
-		printf("Check your input \n");
-		break;
-	}*/
-	insertionsort();
-}
-
-void quicksort() {
-	
+void quickSort(railway* arr, int first, int last) {
+	if (first < last) {
+		int left = first, right = last;
+		int pivon = arr[(left + right) / 2].time;
+		do {
+			while (arr[left].time < pivon) left++;
+			while (arr[right].time > pivon) right--;
+			if (left <= right) {
+				swap(arr[left], arr[right]);
+				left++;
+				right--;
+			}
+		} while (left <= right);
+		quickSort(arr, first, right);
+		quickSort(arr, left, last);
+	}
 }
 
 void insertionsort() {
@@ -170,9 +193,49 @@ void insertionsort() {
 			swap(railwayArray[j - 1], railwayArray[j]);
 		}
 	}
-	for (int i = 0; i < fieldsAmount; i++)
-		printf("%s %d %d %d\n", railwayArray[i].finalDestination, railwayArray[i].numberTrain,
-			railwayArray[i].freeSeats, railwayArray[i].time);
+	print();
+	fclose(file);
+}
+
+void bruteForceSearch() {
+	openFile("r");
+	readFile();
+	int key;
+	printf("Enter time: ");
+	cin >> key;
+	for (int i = 0; i < fieldsAmount; i++) {
+		if (railwayArray[i].time == key) {
+			printf("%s %d %d %d\n", railwayArray[i].finalDestination, railwayArray[i].numberTrain,
+				railwayArray[i].freeSeats, railwayArray[i].time);
+		}
+	}
+	fclose(file);
+	
+}
+void binarySearch(){
+	int key;
+	printf("Enter time: ");
+	cin >> key;
+	int left = 0, right = fieldsAmount - 1, center;
+	while (left <= right) {
+		center = (left + right) / 2;
+		if (key > railwayArray[center].time) {
+			left = center + 1;
+		}
+		else {
+			right = center;
+		}
+		if (railwayArray[left].time == key) {
+			printf("%s %d %d %d\n", railwayArray[left].finalDestination, railwayArray[left].numberTrain,
+				railwayArray[left].freeSeats, railwayArray[left].time);
+			break;
+
+		}
+		else {
+			printf("Nothing found\n");
+			break;
+		}
+	}
 }
 
 
